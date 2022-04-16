3dfff4486a744eed9eed9ce7f297d0aa183e0eae
Милана Иванова <ivanova544252@mail.ru>
2022-04-17 00:46:16 +0900

    get_empty_question_id

--- answer.php
+++ answer.php
@@ -9,8 +9,15 @@ function get_question($id, $connect) {
     $query = pg_query($connect, $command);
     $questions = pg_fetch_all($query);
     //$id = rand(1, sizeof($questions));
-    $command = "SELECT quest  FROM questions where que_id = '$id'";
+    $command = "SELECT quest  FROM questions where id = '$id'";
     $query = pg_query($connect, $command);
     $result = pg_fetch_all($query);
     echo $result[0]['quest'];
+}
+
+function get_empty_question_id($id, $connect){
+    $query = pg_query($connect, "select id from questions left join empl_quest on questions.id = empl_quest.que_id where empl_quest.que_id is null");
+    $empty_questions = pg_fetch_all($query);
+    $var = $empty_questions[rand(0, count($empty_questions))];
+    return $var['id'];
 }
